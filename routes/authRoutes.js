1f58381a650134df66fda40124b656ab4277ef1b
Atik2788 <ashanto653.2@gmail.com>
2025-02-09 07:08:15 +0600

    35 starting seller register and login backend part

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ const router = require('express').Router()
 
  router.post('/admin-login', authControllers.admin_login)
  router.get('/get-user', authMiddleware, authControllers.getUser)
+ router.post('/seller-register', authControllers.admin_login)
 
 
  module.exports = router
