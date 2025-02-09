69591f42f528a6faa39aec3dc8b0a6ce4ea35782
Atik2788 <ashanto653.2@gmail.com>
2025-02-09 21:43:12 +0600

    35 done seller register and login

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,6 @@
 const adminModel = require('../models/adminModel');
+const sellerModel = require('../models/sellerModel');
+const sellerCustomerModel = require('../models/chat/sellerCustomerModel');
 const { responseReturn } = require('../utiles/response');
 const bcrpty = require("bcrypt");
 const { createToken } = require('../utiles/tokenCreate');
@@ -40,10 +42,76 @@ class authControllers{
     // End admin_login method
 
 
+    seller_login = async(req, res) =>{
+        // console.log(req.body);
+        const {email, password} = req.body;
+
+        try {
+            const seller = await sellerModel.findOne({email}).select("+password")
+            // console.log(seller);
+
+            if (seller) {
+                const match = await bcrpty.compare(password, seller.password)
+                // console.log('match', match);
+                if (match) {
+                    const token = await createToken({
+                        id: seller.id,
+                        role: seller.role
+                    })
+                    res.cookie('accessToken', token,{
+                        expires: new Date(Date.now() + 7*24*60*60*1000)
+                    })
+                    responseReturn(res, 200, {token, message: "Login Success!"})
+                }
+                else{
+                    responseReturn(res, 404, {error: "Password not match"})
+                }
+            }
+            else{
+                responseReturn(res, 404, {error: "Email not found"})
+            }
+
+        } catch (error) {
+            responseReturn(res, 500, {error: error.message})
+        }
+    }
+    // End seller_login method
+
+
 
     seller_register = async(req, res) =>{
         const {email, name, password} = req.body;
+
+        try {
+            const getUser = await sellerModel.findOne({email});
+            if(getUser){
+                responseReturn(res, 404, {error: 'Email Already  Exsist!'})
+            }
+            else{
+               const seller = await sellerModel.create({
+                name,
+                email,
+                password: await bcrpty.hash(password, 10),
+                method: 'menualy',
+                shopInfo: {}
+               }) 
+            //    console.log(seller);
+            await sellerCustomerModel.create({
+                myId: seller.id
+            })
+            const token = await createToken({id : seller.id, role: seller.role})
+            res.cookie('accessToken', token, {
+                expires: new Date(Date.now() + 7*24*60*60*1000)
+            })
+
+            responseReturn(res, 201, {token, message: 'Register Success!'})
+            }
+
+        } catch (error) {
+            responseReturn(res, 500, {error: "Internal Server Error"})
+        }
     }
+        // End seller_register method
     
 
 
