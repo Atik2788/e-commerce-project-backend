a3b9746778439224e457bd9e7d5c7a830d6185f3
Atik2788 <ashanto653.2@gmail.com>
2025-02-10 22:42:58 +0600

    36 starting user role manage part

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -6,6 +6,7 @@ const bcrpty = require("bcrypt");
 const { createToken } = require('../utiles/tokenCreate');
 
 class authControllers{
+
     admin_login = async(req, res) =>{
         // console.log(req.body);
         const {email, password} = req.body;
@@ -118,17 +119,17 @@ class authControllers{
     getUser = async(req, res) =>{
         const {id, role} = req;
 
-
         try {
             if (role === 'admin') {
                 const user = await adminModel.findById(id);
                 responseReturn(res, 200, {userInfo: user})
             } else {
-                console.log('Seller info');
+                const seller = await sellerModel.findById(id);
+                responseReturn(res, 200, {userInfo: seller})
             }
             
         } catch (error) {
-            console.log(error.message);
+            responseReturn(res, 500, {error: "Internal Server Error"})
         }
     }
     // End getUser method
