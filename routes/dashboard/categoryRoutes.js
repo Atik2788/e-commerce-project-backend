62c3920bcc7b99f1c7deb2a42497e052e522e3b4
Atik2788 <ashanto653.2@gmail.com>
2025-02-13 23:26:54 +0600

    38 done img push  cloudinary and link set in mongodb

--- routes/dashboard/categoryRoutes.js
+++ routes/dashboard/categoryRoutes.js
@@ -3,6 +3,7 @@ const { authMiddleware } = require('../../middlewares/authMiddlewares')
 const router = require('express').Router()
 
  router.post('/category-add',authMiddleware, categoryController.add_category)
+ router.get('/category-get',authMiddleware, categoryController.get_category)
 
 
  module.exports = router
