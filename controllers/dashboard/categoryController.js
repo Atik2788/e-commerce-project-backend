3b173b89bb1cb96f4141d24664aa066406c02a45
Atik2788 <ashanto653.2@gmail.com>
2025-02-13 09:09:18 +0600

    38 working on add category

---
+++ controllers/dashboard/categoryController.js
@@ -0,0 +1,11 @@
+
+class categoryController{
+
+    add_category = async(req, res) =>{
+        console.log('add category working');
+    }
+
+}
+
+
+module.exports = new categoryController()
---
+++ routes/dashboard/categoryRoutes.js
@@ -0,0 +1,8 @@
+const categoryController = require('../../controllers/dashboard/categoryController')
+const { authMiddleware } = require('../../middlewares/authMiddlewares')
+const router = require('express').Router()
+
+ router.post('/category-add',authMiddleware, categoryController.add_category)
+
+
+ module.exports = router
