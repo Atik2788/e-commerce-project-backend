1d2235d286b5e51f6441610cbbd729826292ac78
Atik2788 <ashanto653.2@gmail.com>
2025-03-05 23:13:31 +0600

    46 get the user data by it

--- controllers/dashboard/sellerController.js
+++ controllers/dashboard/sellerController.js
@@ -1,14 +1,43 @@
 const formidable = require("formidable");
 const { responseReturn } = require("../../utiles/response");
 const cloudinary = require('cloudinary').v2;
-const categoryModel = require('../../models/categoryModel')
+const categoryModel = require('../../models/categoryModel');
+const sellerModel = require("../../models/sellerModel");
 
 class sellerController{
 
     request_seller_get = async(req, res) =>{
-        console.log(req.query);
+        const {page, searchValue, parPage } = req.query;
+        const skipPage = parseInt(parPage) * (parseInt(page) -1)
+
+        try {
+            if(searchValue){
+
+            }
+            else{
+                const sellers = await sellerModel.find({status:'pending'}).skip(skipPage).limit(parPage).sort({createdAt: -1})
+                const totalSeller = await sellerModel.find({status: 'pending'}).countDocuments()
+                responseReturn(res, 200,{sellers, totalSeller})
+            }
+        } catch (error) {
+            responseReturn(res, 500,{error: error.message}) 
+        }
     }
-    //  ******* end get_category method  ***********
+    //  ******* end request_seller_get method  ***********
+
+
+    get_seller = async(req, res) =>{
+        const {sellerId} = req.params;
+        // console.log(sellerId);
+
+        try {
+            const seller = await sellerModel.findById(sellerId)
+            responseReturn(res, 200,{seller})
+        } catch (error) {
+            responseReturn(res, 500,{error: error.message}) 
+        }
+    }
+    //  ******* end get_seller method  ***********
 
 
 }
